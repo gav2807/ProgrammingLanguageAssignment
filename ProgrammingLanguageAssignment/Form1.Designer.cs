fbbb267316602e15cdb0ee53b066e0c26684612a
MSI\P4 <pauludomi@gmail.com>
2022-10-18 22:52:53 +0100

    created my drawing pen and brush for x and Y axis

--- ProgrammingLanguageAssignment/Form1.Designer.cs
+++ ProgrammingLanguageAssignment/Form1.Designer.cs
@@ -28,13 +28,80 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.btnSyntax = new System.Windows.Forms.Button();
+            this.programWindow = new System.Windows.Forms.TextBox();
+            this.commandLine = new System.Windows.Forms.TextBox();
+            this.displayPanel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(12, 252);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(75, 23);
+            this.btnRun.TabIndex = 0;
+            this.btnRun.Text = "Run";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // btnSyntax
+            // 
+            this.btnSyntax.Location = new System.Drawing.Point(130, 252);
+            this.btnSyntax.Name = "btnSyntax";
+            this.btnSyntax.Size = new System.Drawing.Size(75, 23);
+            this.btnSyntax.TabIndex = 1;
+            this.btnSyntax.Text = "Syntax";
+            this.btnSyntax.UseVisualStyleBackColor = true;
+            // 
+            // programWindow
+            // 
+            this.programWindow.Location = new System.Drawing.Point(12, 12);
+            this.programWindow.Multiline = true;
+            this.programWindow.Name = "programWindow";
+            this.programWindow.Size = new System.Drawing.Size(325, 184);
+            this.programWindow.TabIndex = 2;
+            // 
+            // commandLine
+            // 
+            this.commandLine.Location = new System.Drawing.Point(12, 215);
+            this.commandLine.Name = "commandLine";
+            this.commandLine.Size = new System.Drawing.Size(325, 22);
+            this.commandLine.TabIndex = 3;
+            // 
+            // displayPanel
+            // 
+            this.displayPanel.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.displayPanel.Location = new System.Drawing.Point(397, 12);
+            this.displayPanel.Name = "displayPanel";
+            this.displayPanel.Size = new System.Drawing.Size(391, 184);
+            this.displayPanel.TabIndex = 4;
+            this.displayPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.displayPanel_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.displayPanel);
+            this.Controls.Add(this.commandLine);
+            this.Controls.Add(this.programWindow);
+            this.Controls.Add(this.btnSyntax);
+            this.Controls.Add(this.btnRun);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.Button btnSyntax;
+        private System.Windows.Forms.TextBox programWindow;
+        private System.Windows.Forms.TextBox commandLine;
+        private System.Windows.Forms.Panel displayPanel;
     }
 }
 
--- ProgrammingLanguageAssignment/Form1.cs
+++ ProgrammingLanguageAssignment/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +13,64 @@ namespace ProgrammingLanguageAssignment
 {
     public partial class Form1 : Form
     {
+        public static int xbrush = 100;
+        public static int ybrush = 80;
+
+        public static int width = 40;
+        public static int height = 40;
+
+        public static int xpen = (Form1.xbrush - (Form1.width / 2)) + 2;
+        public static int ypen = (Form1.ybrush - (Form1.height / 2)) + 2;
+
+
         public Form1()
         {
             InitializeComponent();
+        
+        }
+
+
+    
+        public static void drawCircle ()
+        {
+           //Pen penPointer = Form1.penPointer();
+           // Graphics graphics = displayPanel.CreateGraphics();
+            //graphics.DrawEllipse(penPointer, 20, 20, 50, 50);
+        }
+
+        public static void drawRectangle()
+        {
+           // Pen penPointer = Form1.penPointer();
+            //Graphics graphics = displayPanel.CreateGraphics();
+            //graphics.DrawRectangle(penPointer, 20, 20, 50, 50);
+        }
+        /*
+        public void drawTriangle()
+        {
+            Pen penPointer = Form1.penPointer();
+            Graphics graphics = displayPanel.CreateGraphics();
+            graphics.Draw(penPointer, 20, 20, 50);
+        }
+        */
+        public static Pen penPointer()
+        {
+            Pen pen = new Pen(Color.Red);
+            return pen;
+
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void displayPanel_Paint(object sender, PaintEventArgs e)
+        {
+            Pen penPointer = Form1.penPointer();
+            SolidBrush brush = new SolidBrush(Color.Red);
+            Graphics graphics = displayPanel.CreateGraphics();
+            graphics.FillEllipse(brush, xbrush, ybrush, 4, 4);
+            graphics.DrawEllipse(penPointer, xpen, ypen, width, height);
         }
     }
 }
