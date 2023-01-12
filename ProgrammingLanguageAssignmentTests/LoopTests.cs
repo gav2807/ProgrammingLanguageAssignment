222af83e1ff9ce8e01f6405afece86c29fb75b45
MSI\P4 <pauludomi@gmail.com>
2023-01-12 23:30:46 +0000

    created loop test

---
+++ ProgrammingLanguageAssignmentTests/LoopTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProgrammingLanguageAssignment;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingLanguageAssignment.Tests
+{
+    [TestClass()]
+    public class LoopTests
+    {
+        [TestMethod()]
+        public void LoopTest()
+        {
+            int value = 10;
+            int variable = 5;
+            string operatorValue = ">";
+            bool loopValue = true;
+
+            Loop loop = new Loop();
+            loop.set(loopValue, variable, value, operatorValue);
+
+            Assert.IsFalse(loop.checkLoopLogic());
+
+        }
+    }
+}
